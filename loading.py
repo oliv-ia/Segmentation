7e7340ad6f901a35cc71aff5d52659a8b1b6cd31
oliv-ia <74462115+oliv-ia@users.noreply.github.com>
2021-10-18 17:10:22 +0100

    Images now display in a grid

--- loading.py
+++ loading.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 from scipy import ndimage
 from pathlib import Path
+from mpl_toolkits.axes_grid1 import ImageGrid
 
 def GetFiles(filedir):
     entries = Path(filedir)
@@ -34,23 +35,40 @@ def GetSliceNumber(mask):
   return slice_number
 
 def main():
-
-    cts, ids, pathlist_ct  = GetFiles("C:\\Users\\Olivia\\Documents\\PhD\\Imaging\\Segmentations\\images")#
-
-    print("path: ", cts, "ids: ", ids, "pathlist :", pathlist_ct)
-    x = DicomToArray(pathlist_ct[1])
+    # extracting CT scans, creating CT array
+    cts_fname, ids, pathlist_ct  = GetFiles("C:\\Users\\Olivia\\Documents\\PhD\\Imaging\\Segmentations\\images")#
+    cts = []
+    for i in range(0, len(pathlist_ct)):
+      x = DicomToArray(pathlist_ct[i])
+      cts.append(x)
     
+    # Extracting masks, creating mask array
+    masks_fname, ids, pathlist_mask = GetFiles("C:\\Users\\Olivia\\Documents\\PhD\\Imaging\\Segmentations\\masks")
+    masks, slice_numbers  = [],[]
+    for i in range(0, len(pathlist_mask) ):
+      y = DicomToArray(pathlist_mask[i])
+      masks.append(y)
+      slice_number = GetSliceNumber(y)
+      slice_numbers.append(slice_number)
+    
+    # dispalying images
+    fig=plt.figure(figsize=(10, 120))
+    ax = []
+    columns = 4
+    rows = int(len(cts)/columns + 0.5)
+ 
+    print("len cts: ",len(cts))
+    for i in range(0, len(cts)):
+      ax.append(fig.add_subplot(rows,columns, i+1))
+      masks[i][masks[i]==0] = np.nan
+      plt.imshow(cts[i][slice_numbers[i],:,:], cmap="gray")
+      plt.imshow(masks[i][slice_numbers[i],:,:], cmap = "autumn", alpha = 0.7)
 
-    masks, ids, pathlist_mask = GetFiles("C:\\Users\\Olivia\\Documents\\PhD\\Imaging\\Segmentations\\masks")
-    y = DicomToArray(pathlist_mask[1])
-    slice_number = GetSliceNumber(y)
-    print("slice_no: ", slice_number)
-
-    #im = plt.imshow(x[slice_number], cmap = "gray")
-    #plt.show()
-    im = plt.imshow(y[slice_number], cmap= "gray")
+      ax[-1].set_title("Patient:" + str(ids[i]))
+      plt.axis("off")
+    plt.tight_layout(True)
     plt.show()
-    
+
     return 
 
 if __name__ == '__main__':
