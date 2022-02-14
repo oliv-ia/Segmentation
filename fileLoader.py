72f9f5f087640eabd7c24b2be637a0f162a3eff5
Olivia <olivianmurray1999@gmail.com>
2022-02-14 15:03:43 +0000

    Added seaborn plots

--- fileLoader.py
+++ fileLoader.py
@@ -191,6 +191,7 @@ def DiceCaster(staple_array, staple_ids, staple_slices, masks_a, ids_a):
             
             #print("place holder: ", place_holder, "ind: ", ind, "i: ", i, "j: ", j )
             dice_coeff = compute_dice_coefficient(masks_a[place_holder][ind,:,:], staple_array[i][j])
+            dice_coeff = dice_coeff * 100
             dices_array.append(dice_coeff)
         dice_array.append(dices_array)   
     return dice_array
@@ -210,9 +211,11 @@ def AverageDice(dice_array):
     total_avg = sum/length
     return total_avg, average
 
-def GetSliceOverlap(slices_a, ids_a, slices_b, ids_b, slices_c, ids_c):
+def GetSliceOverlap(masks_a, ids_a, masks_b, ids_b, masks_c, ids_c):
     percent = []
-    
+    slices_a = GetSlicesArray(masks_a)
+    slices_b = GetSlicesArray(masks_b)
+    slices_c = GetSlicesArray(masks_c)
     total_intersection = 0
     total_union = 0
     for i in ids_a:
@@ -246,20 +249,47 @@ def main():
 
 
     new_mask_paul,new_id_paul,new_mask_adrian,new_id_adrian,new_mask_sacha,new_id_sacha = IdSkip(ids_paul, ids_adrian, ids_sacha, masks_paul, masks_adrian, masks_sacha)
+    total_percentage, percentage = GetSliceOverlap(new_mask_paul,new_id_paul,new_mask_adrian,new_id_adrian,new_mask_sacha,new_id_sacha)
+    slices_df = pd.DataFrame(percentage, columns = ["Slice IOU"], index = new_id_adrian)
+    slices_df["Total IOU"] = total_percentage
+    slices_df.index.name = "Patient ID"
     staple_array, staple_ids, staple_slices = StapleGun(masks_a= new_mask_adrian, masks_b= new_mask_paul, masks_c= new_mask_sacha, ids_a= new_id_adrian, ids_b = new_id_paul, ids_c= new_id_sacha)
 
     dice_array_adrian = DiceCaster(staple_array, staple_ids, staple_slices, new_mask_adrian, new_id_adrian)
     adrian_total_avg, adrian_patient_avg = AverageDice(dice_array_adrian)
+    adrian_df = pd.DataFrame(dice_array_adrian, columns = ["Dice 1", "Dice 2", "Dice 3", "Dice 4" ,"Dice 5" , "Dice 6"] , index = staple_ids)
+    #adrian_df.insert(0, "Patient_ID", new_id_adrian, True)
+    adrian_df["Average Dice"] = adrian_patient_avg
+    adrian_df.index.name = "Patient ID"
+
 
     dice_array_paul = DiceCaster(staple_array, staple_ids, staple_slices, new_mask_paul, new_id_paul)
     paul_total_avg, paul_patient_avg = AverageDice(dice_array_paul)
 
+    paul_df = pd.DataFrame(dice_array_paul, columns = ["Dice 1", "Dice 2", "Dice 3", "Dice 4", "Dice 5", "Dice 6"], index = staple_ids)
+    #paul_df.insert(0, "Patient_ID", new_id_paul, True)
+    paul_df["Average Dice"] = paul_patient_avg
+    paul_df.index.name = "Patient ID"
+
     dice_array_sacha = DiceCaster(staple_array, staple_ids, staple_slices, new_mask_sacha, new_id_sacha)
     sacha_total_avg, sacha_patient_avg = AverageDice(dice_array_sacha)
 
+    sacha_df = pd.DataFrame(dice_array_sacha, columns = ["Dice 1", "Dice 2", "Dice 3", "Dice 4" ,"Dice 5", "Dice 6"], index = staple_ids)
+    #sacha_df.insert(0, "Patient_ID", new_id_sacha, True)
+    sacha_df["Average Dice"] = sacha_patient_avg
+    sacha_df.index.name = "Patient ID"
+
+
+    total_df = pd.DataFrame([adrian_total_avg, paul_total_avg, sacha_total_avg], index =["Adrian average Dice","Paul average Dice", "Sacha average Dice"] )
+   
     print("avg adrian: ", adrian_total_avg, "avg paul: ", paul_total_avg, "avg sacha: ", sacha_total_avg)
 
-
+    with pd.ExcelWriter('/Users/olivia/Documents/PhD/MISTIE/mask_data/output.xlsx') as writer:  
+        adrian_df.to_excel(writer, sheet_name='Adrian')
+        paul_df.to_excel(writer, sheet_name='Paul')
+        sacha_df.to_excel(writer, sheet_name='Sacha')
+        total_df.to_excel(writer, sheet_name='Total Dice')
+        slices_df.to_excel(writer,sheet_name ='Slices' )
 
 
 
